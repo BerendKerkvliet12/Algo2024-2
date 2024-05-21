c6ba1680b805c8e3166d971b8f3a8fb97fc595c5
Berend <berend.kerkvliet@icloud.com>
2024-05-21 12:18:20 +0200

    nog paar functions

--- Assignment2/assignment2_2361353_3326500_notebook_backup.py
+++ Assignment2/assignment2_2361353_3326500_notebook_backup.py
@@ -180,5 +180,196 @@ class FloodFillSolver():
                     valid_steps.append((nx, ny))
         return valid_steps
 
+############ CODE BLOCK 10 ################
+
+class GraphBluePrint():
+    """
+    You can ignore this class, it is just needed due to technicalities.
+    """
+    def find_nodes(self): pass
+    def find_edges(self): pass
+    
+class Graph(GraphBluePrint):   
+    """
+    Attributes:
+        :param adjacency_list: The adjacency list with the road distances and speed limit.
+        :type adjacency_list: dict[tuple[int]: set[edge]], where an edge is a fictional datatype 
+                              which is a tuple containing the datatypes tuple[int], int, float
+        :param map: The map of the graph.
+        :type map: Map
+    """
+    def __init__(self, map_, start=(0, 0)):
+        """
+        This function transforms any (city or lower) map into a graph representation.
+
+        :param map_: The map that needs to be transformed.
+        :type map_: Map
+        :param start: The start node from which we will find all other nodes.
+        :type start: tuple[int]
+        """
+        self.adjacency_list = {}
+        self.map = map_
+        self.start = start
+        
+        self.find_nodes()
+        self.find_edges()  # This will be implemented in the next notebook cell
+        
+    def find_nodes(self):
+        """
+        This method contains a breadth-frist search algorithm to find all the nodes in the graph.
+        So far, we called this method `step`. However, this class is more than just the search algorithm,
+        therefore, we gave it a bit more descriptive name.
+
+        Note, that we only want to find the nodes, so history does not need to contain a partial path (previous node).
+        In `find_edges` (the next cell), we will add edges for each node.
+        """
+        queue = deque([self.start])
+        history = {self.start}
+        self.adjacency_list[self.start] = set()
+        while queue:
+            current_node = queue.popleft()
+            neighbours = self.neighbour_coordinates(current_node)
+            for neighbour in neighbours:
+                if neighbour not in history:
+                    history.add(neighbour)
+                    queue.append(neighbour)
+                    self.adjacency_list_add_node(neighbour, neighbours)
+        
+        
+                    
+    def adjacency_list_add_node(self, coordinate, actions):
+        """
+        This is a helper function for the breadth-first search algorithm to add a coordinate to the `adjacency_list` and
+        to determine if a coordinate needs to be added to the `adjacency_list`.
+
+        Reminder: A coordinate should only be added to the adjacency list if it is a corner, a crossing, or a dead end.
+                  Adding the coordinate to the adjacency_list is equivalent to saying that it is a node in the graph.
+
+        :param coordinate: The coordinate that might need to be added to the adjacency_list.
+        :type coordinate: tuple[int]
+        :param actions: The actions possible from this coordinate, an action is defined as an action in the coordinate state-space.
+        :type actions: list[tuple[int]]
+        """
+        if len(actions) > 2: # If the coordinate is a corner or crossing
+            self.adjacency_list[coordinate] = set()
+        if len(actions) == 0: # If the coordinate is a dead end
+            self.adjacency_list[coordinate] = set()
+                           
+    def neighbour_coordinates(self, coordinate):
+        """
+        This method returns the next possible actions and is part of the breadth-first search algorithm.
+        Similar to `find_nodes`, we often call this method `next_step`.
+        
+        :param coordinate: The current coordinate
+        :type coordinate: tuple[int]
+        :return: A list with possible next coordinates that can be visited from the current coordinate.
+        :rtype: list[tuple[int]]  
+        """
+        max_height, max_width = self.map.shape
+        possible_moves = [
+            (coordinate[0] + 1, coordinate[1]),  # Move down
+            (coordinate[0] - 1, coordinate[1]),  # Move up
+            (coordinate[0], coordinate[1] + 1),  # Move right
+            (coordinate[0], coordinate[1] - 1)   # Move left
+        ]
+        # Check if the moves are within the map's boundaries
+        return [move for move in possible_moves if 0 <= move[0] < max_height and 0 <= move[1] < max_width and self.map.grid[move[0], move[1]] != 0]
+    
+    def __repr__(self):
+        """
+        This returns a representation of a graph.
+
+        :return: A string representing the graph object.
+        :rtype: str
+        """
+        # You can change this to anything you like, such that you can easily print a Graph object. An example is already given.
+        return repr(dict(sorted(self.adjacency_list.items()))).replace("},", "},\n")
+
+    def __getitem__(self, key):
+        """
+        A magic method that makes using keys possible.
+        This makes it possible to use self[node] instead of self.adjacency_list[node]
+
+        :return: The nodes that can be reached from the node `key`.
+        :rtype: set[tuple[int]]
+        """
+        return self.adjacency_list[key]
+
+    def __contains__(self, key):
+        """
+        This magic method makes it possible to check if a coordinate is in the graph.
+
+        :return: This returns if the coordinate is in the graph.
+        :rtype: bool
+        """
+        return key in self.adjacency_list
+
+    def get_random_node(self):
+        """
+        This returns a random node from the graph.
+        
+        :return: A random node
+        :rtype: tuple[int]
+        """
+        return tuple(RNG.choice(list(self.adjacency_list)))
+        
+    def show_coordinates(self, size=5, color='k'):
+        """
+        If this method is used before another method that does a plot, it will be plotted on top.
+
+        :param size: The size of the dots, default to 5
+        :type size: int
+        :param color: The Matplotlib color of the dots, defaults to black
+        :type color: string
+        """
+        nodes = self.adjacency_list.keys()
+        plt.plot([n[1] for n in nodes], [n[0] for n in nodes], 'o', color=color, markersize=size)        
+
+    def show_edges(self, width=0.05, color='r'):
+        """
+        If this method is used before another method that does a plot, it will be plotted on top.
+        
+        :param width: The width of the arrows, default to 0.05
+        :type width: float
+        :param color: The Matplotlib color of the arrows, defaults to red
+        :type color: string
+        """
+        for node, edge_list in self.adjacency_list.items():
+            for next_node,_,_ in edge_list:
+                plt.arrow(node[1], node[0], (next_node[1] - node[1])*0.975, (next_node[0] - node[0])*0.975, color=color, length_includes_head=True, width=width, head_width=4*width)
+
+############ CODE BLOCK 15 ################
+    def find_edges(self):
+        """
+        This method does a depth-first/brute-force search for each node to find the edges of each node.
+        """
+        for node in self.adjacency_list.keys():
+            neighbors = self.neighbour_coordinates(node)
+            for neighbor in neighbors:
+                if neighbor in self.adjacency_list:  # Check if the neighbor is also a node
+                    distance = self.map.grid[neighbor]  # Assume distance or weight could be directly from map values
+                    edge = (neighbor, distance)  # Simplified edge definition
+                    self.adjacency_list[node].add(edge)
+
+    def find_next_node_in_adjacency_list(self, node, direction):
+        """
+        This is a helper method for find_edges to find a single edge given a node and a direction.
+
+        :param node: The node from which we try to find its "neighboring node" NOT its neighboring coordinates.
+        :type node: tuple[int]
+        :param direction: The direction we want to search in this can only be 4 values (0, 1), (1, 0), (0, -1) or (-1, 0).
+        :type direction: tuple[int]
+        :return: This returns the first node in this direction and the distance.
+        :rtype: tuple[int], int 
+        """
+        x, y = node
+        dx, dy = direction
+        while 0 <= x < self.map.shape[0] and 0 <= y < self.map.shape[1]:
+            if self.map.grid[x, y] != 0:
+                return (x, y), self.map.grid[x, y]
+            x += dx
+            y += dy
+        return None, 0
+
 
 ############ END OF CODE BLOCKS, START SCRIPT BELOW! ################
